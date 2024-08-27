049f889cccbc4da0798be4328566de7576a46850
Hercolys Moraes Araujo <avanade.hercolys@grupofleury.com.br>
2024-08-27 18:05:22 -0300

    Add project files.

---
+++ DotNETChallengeParkingSystem/Models/Parking.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingSystem.Models
+{
+    public class Parking
+    {
+        public decimal StartingPrice { get; set; }
+        public decimal PricePerHour { get; set; }
+        public List<string> Vehicles { get; set; }
+
+        public Parking(decimal startingPrice, decimal pricePerHour)
+        {
+            StartingPrice = startingPrice;
+            PricePerHour = pricePerHour;
+            Vehicles = new List<string>();
+        }
+
+        /// <summary>
+        /// Adds a vehicle to the parking.
+        /// </summary>
+        /// <param name="vehicle">The vehicle to add.</param>
+        public void AddVehicle(string vehicle)
+        {
+            Vehicles.Add(vehicle);
+            Console.WriteLine("Veículo adicionado com sucesso");
+        }
+
+        /// <summary>
+        /// Removes a vehicle from the parking.
+        /// </summary>
+        /// <param name="vehicle">The vehicle to remove.</param>
+        private bool RemoveVehicle(string vehicle)
+        {
+            if (!Vehicles.Contains(vehicle))
+            {
+                Console.WriteLine("Veículo não encontrado");
+                return false;
+            }
+
+            Vehicles.Remove(vehicle);
+            Console.WriteLine("Veículo removido com sucesso");
+            return true;
+        }
+
+        /// <summary>
+        /// Calculates the price based on the number of hours.
+        /// </summary>
+        /// <param name="numberOfHours">The number of hours the vehicle was parked.</param>
+        private void CalculatePrice(int numberOfHours)
+        {
+            decimal price = StartingPrice + numberOfHours * PricePerHour;
+            Console.WriteLine($"Preço a pagar: {price}");
+        }
+
+        /// <summary>
+        /// Removes a vehicle from the parking and calculates the price based on the number of hours.
+        /// </summary>
+        /// <param name="vehicle">The vehicle to remove.</param>
+        /// <param name="numberOfHours">The number of hours the vehicle was parked.</param>
+        public void RemoveVehicle(string vehicle, int numberOfHours)
+        {
+            if (RemoveVehicle(vehicle))
+            {
+                CalculatePrice(numberOfHours);
+            }
+        }
+
+        /// <summary>
+        /// Lists all the vehicles in the parking.
+        /// </summary>
+        public void ListVehicles()
+        {
+            if (Vehicles.Count == 0)
+            {
+                Console.WriteLine("Nenhum veículo no estacionamento");
+                return;
+            }
+
+            Console.WriteLine("Lista de veículos estacionados:");
+            foreach (var vehicle in Vehicles)
+            {
+                Console.WriteLine(vehicle);
+            }
+        }
+    }
+}
---
+++ DotNETChallengeParkingSystem/Program.cs
@@ -0,0 +1,62 @@
+﻿using ParkingSystem.Models;
+
+Console.WriteLine("Sistema de Estacionamento");
+
+Console.WriteLine("Digite o preço inicial:");
+decimal startingPrice = decimal.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o preço por hora:");
+decimal pricePerHour = decimal.Parse(Console.ReadLine());
+
+Parking parking = new Parking(startingPrice, pricePerHour);
+
+int input = 0;
+
+while (input != 4)
+{
+    Console.WriteLine(
+        "\nO que você gostaria de fazer?\n" +
+        "1 - Cadastrar veículo\n" +
+        "2 - Remover veículo\n" +
+        "3 - Listar veículos\n" +
+        "4 - Encerrar"
+    );
+
+    if (!int.TryParse(Console.ReadLine(), out input))
+    {
+        Console.WriteLine("Opção inválida");
+        continue;
+    }
+   
+    switch (input)
+    {
+        case 1:
+            Console.WriteLine("Digite a placa do veículo:");
+            string plate = Console.ReadLine();
+
+            parking.AddVehicle(plate);
+            break;
+
+        case 2:
+            Console.WriteLine("Digite a placa do veículo:");
+            string plateToRemove = Console.ReadLine();
+
+            Console.WriteLine("Digite o número de horas que o veículo ficou estacionado:");
+            int numberOfHours = int.Parse(Console.ReadLine());
+
+            parking.RemoveVehicle(plateToRemove, numberOfHours);
+            break;
+
+        case 3:
+            parking.ListVehicles();
+            break;
+
+        case 4:
+            Console.WriteLine("Encerrando o sistema");
+            break;
+
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+}
